d0f1d119faeb5d8238ee27b5bf75677fdd9a03c8
theprogrammerin <Ashutosh_Agrawal@live.in>
2012-04-14 22:25:09 +0530

    C Programs on Operating System Algorithms
    	new file:   os/a.out
    	new file:   os/deadlock/a.out
    	new file:   os/deadlock/a.txt
    	new file:   os/deadlock/a.txt~
    	new file:   os/deadlock/b.txt
    	new file:   os/deadlock/b.txt~
    	new file:   os/deadlock/da.c
    	new file:   os/deadlock/da.c~
    	new file:   os/deadlock/ss.c
    	new file:   os/deadlock/ss.c~
    	new file:   os/fact.c
    	new file:   os/fibo.c
    	new file:   os/great3.c
    	new file:   os/os/a.out
    	new file:   os/paging/a.out
    	new file:   os/paging/fifo.c
    	new file:   os/paging/fifo.c~
    	new file:   os/paging/lru.c
    	new file:   os/paging/optimal.c
    	new file:   os/scheduling/a.out
    	new file:   os/scheduling/core
    	new file:   os/scheduling/fcfs.c
    	new file:   os/scheduling/fcfs.c~
    	new file:   os/scheduling/npp.c
    	new file:   os/scheduling/npp.c~
    	new file:   os/scheduling/ps.c
    	new file:   os/scheduling/ps.c~
    	new file:   os/scheduling/rr.c
    	new file:   os/scheduling/rr.c~
    	new file:   os/scheduling/spf.c
    	new file:   os/scheduling/spf.c~
    	new file:   os/scheduling/srt.c
    	new file:   os/scheduling/srt.c~
    	new file:   os/scheduling/test.c
    	new file:   os/scheduling/test.c~
    	new file:   os/scheduling/test.o
    	new file:   os/threading/a.out
    	new file:   os/threading/pth_join.c
    	new file:   os/threading/pth_join.c~
    	new file:   os/threading/s_th.c
    	new file:   os/threading/th_arg.c
    	new file:   os/threading/th_arg.c~

---
+++ os/deadlock/da.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <math.h>
+
+#define pi(x)	printf("%d",x)
+#define	si(x)	scanf("%d",&x)
+#define ps(x)	printf("%s",x)
+	
+	int claim[10][10],alloc[10][10],resrc[10],avail[10],req[10][10];
+	int m,n;
+	
+int safe()
+{
+	int i,j,k,c;
+	int f1,f2,f3,f4;
+	int sp;
+	c = m;
+	while(c>0)
+	{
+		f2 = 0;
+		for(i=0;i<m;i++)
+		{
+			f1 = 0;
+			f3 = 1;
+			for(j=0;j<n;j++)
+			{
+				if(claim[i][j] == 0 && f3==1)
+					f3 = 1;
+				else
+					f3 = 0;
+			}
+			if(f3)
+				continue;
+			f4 = (int)Math.pow(2,10);
+			for(j=0;j<i;j++)
+			{
+				f3 = 0
+				for(k=m-1;k>=0;k--)
+				{
+					f3 += (int)Math.pow(2,(m-k-1)); 
+				}
+				if(f3 < f4 )
+				{
+					f4 = f3;
+					sp = j;
+				}
+			}
+			
+			for(j=0;j<n;j++)
+			{
+				if(alloc[sp][j] + avail[j] >= claim[sp][j]) //checking for requirements
+					f1 = 1; 
+				else
+				{
+					break;
+				}
+			}
+			if(f1)
+			{
+				ps("\nProcess");
+				pi((i+1));
+				ps(" Executed");
+				for(j=0; j<n; j++)
+				{
+					claim[sp][j] = 0;
+					avail[j] += alloc[sp][j];
+					alloc[sp][j] = 0;
+					req[sp][j] = 0;
+				}
+				f2 = 1;
+				break;
+			}
+		}
+		if(!f2)
+		{
+			printf("\nDeadlock Occured between Process ");
+			for(i=0;i<m;i++)
+			{
+				f3 = 1;
+				for(j=0;j<n;j++)
+				{
+					if(claim[i][j] == 0 && f3==1)
+						f3 = 1;
+					else
+						f3 = 0;
+				}
+				if(!f3)
+					printf("%d ",(i+1));
+			}
+			return 0;
+			break;
+		}
+		else
+		{
+			c--;
+			ps("\nAvailable Vector : ");
+			for(i = 0; i<n; i++)
+			{
+				pi(avail[i]);
+			}
+			ps("\n");
+		}
+	}
+	if(c==0)
+		return 1;
+}
+int main()
+{
+	int i,j,k,c;
+	ps("Enter Number of Process : ");
+	si(m);
+	ps("Enter Number of Resources :");
+	si(n);
+	ps("Enter Claim Matrix : \n");
+	for(i = 0; i<m; i++)
+	{
+		printf("Process %d :",(i+1));
+		for(j=0;j<n;j++)
+			si(claim[i][j]);
+		ps("\n");
+	}
+	ps("Enter Allocation Matrix : \n");
+	for(i = 0; i<m; i++)
+	{
+		printf("Process %d :",(i+1));
+		for(j=0;j<n;j++)
+		{
+			si(alloc[i][j]);
+			req[i][j] = claim[i][j] - alloc[i][j];
+		}
+		ps("\n");
+	}
+	ps("Enter Resource Vector : ");
+	for(i = 0; i<n; i++)
+	{
+		si(resrc[i]);
+		avail[i] = resrc[i];
+		for(j=0;j<m;j++)
+		{
+			avail[i] -= alloc[j][i]; 
+		}
+	}
+	ps("\n");
+	
+	if(safe()==0)
+	{
+		ps("\nNot Safe\n");
+	}
+	else
+	{
+		ps("\nSafe\n");
+	}
+	return 0;
+}
